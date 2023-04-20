9304daf2ae6dca48c1c5a0da7a9f5924da003683
PernillePersson <78405936+PernillePersson@users.noreply.github.com>
2023-04-20 11:43:35 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.lang.Math;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    private int arrival;
+    private int service;
+
+    public static void main(String[] args) {
+
+
+        int min = 1;
+        int max = 4;
+
+        int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
+        int dayshift = 720;
+
+        Queue<Customer> checkoutQueue = new LinkedList();
+
+
+        /*for (tid = 0; tid < dayshift; tid++){
+
+        }*/
+
+
+
+    }
+}
